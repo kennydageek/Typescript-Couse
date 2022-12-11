1b6b302f44581d79de467e2af8445cae4d38de15
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-12-11 22:00:58 +0100

    annotations for anonymous function

--- 02. Features/annotations/functions.ts
+++ 02. Features/annotations/functions.ts
@@ -5,3 +5,11 @@ const add = (a: number, b: number): number => {
 const subract = (a: number, b: number): number => {
   return a - b;
 };
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
